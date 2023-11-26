5e19414871daded1e46be9f664ace52d412b9201
izumi0978 <izuizumen1108@gmail.com>
2023-11-26 23:34:41 +0900

    1126

---
+++ resources/views/Hello.blade.php
@@ -0,0 +1,15 @@
+<?php
+  $test_php = 'こんにちは'
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php echo $test_php?>
+</body>
+</html>
